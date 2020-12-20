e7610b29ac502dd1364d182a12619dbf83bab384
Laura Fischer <lauramaria.fischer@stud-inf.unibz.it>
2020-12-20 12:58:11 +0100

    delete update

--- navbar.php
+++ navbar.php
@@ -3,10 +3,9 @@
         margin-left: 20px; 
     }
 
-    #delete {
-        padding-left: 500px;
+    #logout {
+        margin-left: 700px;
     }
-
 </style>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -15,7 +14,6 @@
         <a class="nav-item nav-link active" href="allPosts.php">All posts<span class="sr-only">(current)</span></a>
         <a class="nav-item nav-link" href="myPosts.php">My posts</a>
         <a class="nav-item nav-link" href="addPost.php">Add post</a>
-        <a class="nav-item nav-link" id="delete" href="delete.php" onclick="return confirm('Are you sure you want to delete this comment?');">Delete Account</a>
         <a class="nav-item nav-link" id="logout" href="logout.php">Logout</a>
     </div>
 </nav>
